d788efc8a8b52c1b4477bdeb523a7282cb15cd8e
Roy M J <roymj88@gmail.com>
2015-07-24 22:38:26 +0530

    Modify JS to remove inline config function usage

--- feed/feed.js
+++ feed/feed.js
@@ -1,18 +1,20 @@
 (function() {
 
-	angular.module('feed', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+	angular.module('feed', ['ui.bootstrap', 'ui.utils',	'ui.router', 'ngAnimate' ]);
 
-	angular.module('feed').config(function($stateProvider) {
 
-	    $stateProvider.state('list', {
+	function config($stateProvider){
+		$stateProvider.state('list', {
 	        url: '/list',
 	        templateUrl: 'feed/list/list.html',
 	        controller: 'ListCtrl',
 	        controllerAs: 'list'
 	    });
 	    /* Add New States Above */
+	}
 
-	});
+
+	angular.module('feed').config(config);
 
 })();
 
