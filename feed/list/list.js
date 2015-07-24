807ff6fcc348951c212d17727cc4e017c96fba9f
Roy M J <roymj88@gmail.com>
2015-07-24 22:39:49 +0530

    Modify list controller to remove inline function use

--- feed/list/list.js
+++ feed/list/list.js
@@ -1,26 +1,27 @@
 (function() {
 
-	angular.module('feed').controller('ListCtrl',function($scope, reddit){
+	function ListCtrl($scope, reddit){
 		var vm = this;
 		vm.data = {};
 
 		// Get List Items
-	 	reddit.getItems().then(function(res) {
-	 		if(!res.error){
+		reddit.getItems().then(function(res) {
+			if(!res.error){
 				vm.data.lists = res.data.children;
-	 		}
+			}
 		});
 
-	 	// Get List with respect to Searched keyword
+		// Get List with respect to Searched keyword
 		vm.searchSubmit = function(){
 			var searchKey = vm.search;
 			reddit.searchItems(searchKey).then(function(res) {
 				if(!res.error){
 					vm.data.lists = res.data.children;
-		 		}
+				}
 			});
-		}
+		};
+	}
 
-	});
+	angular.module('feed').controller('ListCtrl', ListCtrl);
 
 })();
