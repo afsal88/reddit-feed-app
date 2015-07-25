f01bbcd73b5d283721d29de661bbc5d73b7d5d0a
Roy M J <roymj88@gmail.com>
2015-07-25 21:03:27 +0530

    Fix grunt errors regarding spacing

--- feed/directive/rd-search.js
+++ feed/directive/rd-search.js
@@ -1,20 +1,18 @@
 (function() {
 
+	// Directive for getting List with respect to Searched keyword
+	// Currently works with enter key submission
 	function rdSearch(reddit) {
 		return {
 			restrict: 'A',
 			link: function(scope, element, attrs, fn) {
 				$(element).keypress(function(event) {
-				  	var searchKey = this.value;
-
-				  	if(event.which === 13) {
-
-					  	reddit.searchItems(searchKey).then(function(res) {
-
+					var searchKey = this.value;
+					if(event.which === 13) {
+						reddit.searchItems(searchKey).then(function(res) {
 							if(!res.error){
 								scope.list.data.lists = res.data.children;
-							}
-							
+							}							
 						});
 					}
 				});
