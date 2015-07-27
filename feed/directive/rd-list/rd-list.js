c88743904661eed5a7a9d88649717275b3d48a3f
Roy M J <roymj88@gmail.com>
2015-07-27 12:22:03 +0530

    Add list view as directive

---
+++ feed/directive/rd-list/rd-list.js
@@ -0,0 +1,10 @@
+angular.module('feed').directive('rdList', function() {
+	return {
+		restrict: 'EA',
+		templateUrl: 'feed/directive/rd-list/rd-list.html',
+		link: function(scope, element, attrs, fn) {
+
+
+		}
+	};
+});
